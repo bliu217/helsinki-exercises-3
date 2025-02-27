482c142c9ed69c5252297ea926abe368e8ff52d4
UBC Student <bliu41@students.cs.ubc.ca>
2025-02-26 23:06:26 -0800

    changed port

--- index.js
+++ index.js
@@ -9,11 +9,11 @@ app.use(express.json())
 app.use(morgan('tiny'))
 
 const Person = require('./models/person')
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
-
+app.use(express.static('dist'))
 
 
 // let persons = [
